a6f9955609ef3398b693e077d4ebaf05ffd11cc5
Артём Межинский <aswert092@gmail.com>
2023-02-13 20:25:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            ThreadStart threadStart = new ThreadStart(ConsoleWrite);
+            Thread thread = new Thread(threadStart);
+            thread.IsBackground = true;
+            thread.Priority = ThreadPriority.Lowest;
+            thread.Start();
+            thread.Join();
+            for (int i=0;i <=50;i++)
+            {
+                Console.WriteLine("Из основной программы: " + i);
+            }
+        }
+        static void ConsoleWrite()
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                if(i == 25)
+                {
+                    Console.ReadLine();
+                }
+                Console.WriteLine("Из потока: " + i);
+            }
+        }
+    }
+}
