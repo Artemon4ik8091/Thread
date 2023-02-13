e534da7ef87d3a525711d9166a778e06c13a0083
Артём Межинский <aswert092@gmail.com>
2023-02-13 20:59:21 +0300

    321

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -4,9 +4,11 @@
     {
         static void Main(string[] args)
         {
-            MyObject myObject= new MyObject();
+            MyObject myObject = new MyObject();
             myObject.Start = Int32.Parse(Console.ReadLine());
             myObject.End = Int32.Parse(Console.ReadLine());
+            Threads threads = new Threads();
+            threads.count = Int32.Parse(Console.ReadLine());
             //Console.WriteLine("Hello, World!");
             ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart(ConsoleWrite);
             //ThreadStart threadStart = new ThreadStart(ConsoleWrite);
@@ -21,8 +23,9 @@
             int Start = ((MyObject)my).Start;
             int End = ((MyObject)my).End;
             string Message = ((MyObject)my).Message;
+            int count = ((Threads)my).count;
             for (int i = Start; i <= End; i++)
-            {               
+            {
                 Console.WriteLine("Из потока: " + i);
             }
         }
@@ -33,5 +36,9 @@
         public int Start { get; set; }
         public int End { get; set; }
         public string Message { get; set; } = "THIS IS CHEATS!";
+    }
+    class Threads
+    {
+        public int count { get; set; }
     }
 }
